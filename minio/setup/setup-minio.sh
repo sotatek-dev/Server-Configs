ef3a86819b5c4713aa446ea875e124b56f79bc0d
Sotatek-HuyLuu <huy.luu@sotatek.com>
2025-08-24 20:35:46 +0700

    feat(minio): create bucket by cli (#18)
    * chore: update config minIo in docker-compose
    
    * chore: update
    
    ---------
    
    Co-authored-by: Andrew <an.nguyen8@sotatek.com>

---
+++ minio/setup/setup-minio.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Delay 15s waiting for MinIO to be ready..."
+sleep 15
+
+echo "Configuring MinIO client..."
+mc alias set myminio http://minio:9000 minio 123@1234
+
+echo "Creating bucket 'ris-bucket'..."
+mc mb myminio/ris-bucket || true
+
+echo "Setting bucket policy to public read..."
+mc anonymous set download myminio/ris-bucket
+
+echo "Setup complete!"
