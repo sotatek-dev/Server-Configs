7e9985e29320603a717edc813d3a8d6194ffaff9
Andrew <an.nguyen8@sotatek.com>
2025-08-29 03:02:31 +0700

    feat(minio): use custom image

--- minio/setup/setup-minio.sh
+++ minio/setup/setup-minio.sh
@@ -2,9 +2,8 @@
 
 echo "Delay 15s waiting for MinIO to be ready..."
 sleep 15
-
 echo "Configuring MinIO client..."
-mc alias set myminio http://minio:9000 minio 123@1234
+mc alias set myminio "$MINIO_ALIAS_URL" "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 
 echo "Creating bucket 'ris-bucket'..."
 mc mb myminio/ris-bucket || true
